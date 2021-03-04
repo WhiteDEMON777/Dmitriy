7c7c9b22ab0ff615b063bb3e2aec9689ba5c749c
DeLineX <temik3t3@yandex.ru>
2021-03-04 22:36:25 +0400

    uyhgjyd

--- js/script.js
+++ js/script.js
@@ -1,77 +1,12 @@
-function pasCount(val, target, adult = false, children = false) {
-    let sum = 0;
-    let pas = $('.modal__countInp');
-    pas.each(function() {
-        sum += +this.value;
-    });
-
-    let amount = +target.val() + val;
-    if (sum + val <= 4 && sum + val >= 0 && amount >= adult ? 1 : 0) {
-        target.val(amount);
-        let cont = $('.modal__children');
-        if (children) {
-            let line = $('.modal__line', cont).eq(-1);
-            if (amount > 0) {
-                cont.slideDown();
-                if (val > 0) {
-                    if (amount > 1) {
-                        line.clone().appendTo(cont);
-                        line = $('.modal__line', cont);
-                        let n = line.length;
-                        line = line.eq(-1);
-                        $('label.modal__checkbox', line).attr('for', 'seat' + n);
-                        $('input[type="checkbox"]', line).attr('id', 'seat' + n);
-                        line.css('display', 'none');
-                        line.slideDown();
-                    }
-                } else {
-                    line.slideUp();
-                    setTimeout(function() {
-                        line.remove();
-                    }, 400);
-                }
-            } else {
-                cont.slideUp();
-            }
-        }
-    }
-}
-
-function addChildrenSelect(remove = false) {}
-
 $(document).ready(function() {
-    //modal__pasengers
-    $('.modal__countMinus').each(function(index, item) {
-        $(item).on('click', function(e) {
-            let cont = $(e.target).parent();
-            let target = $('.modal__countInp', cont);
-            if (target.attr('adult')) {
-                pasCount(-1, target, true);
-            } else if (target.attr('children')) {
-                pasCount(-1, target, false, true);
-            } else {
-                pasCount(-1, target);
-            }
-        });
-    });
-    $('.modal__countPlus').each(function(index, item) {
-        $(item).on('click', function(e) {
-            let cont = $(e.target).parent();
-            let target = $('.modal__countInp', cont);
-            if (target.attr('children')) {
-                pasCount(1, target, false, true);
-            } else {
-                pasCount(1, target);
-            }
-        });
-    });
     
     var start = new Date();
 
     if ($(".dateRide").length) {
-        $(".dateRide").datepicker({
+        var datepicker = $(".dateRide").datepicker({
             dateFormat: 'd MM yyyy',
-        }).data('datepicker').selectDate(new Date());
+        });
+        datepicker.data('datepicker').selectDate(new Date());
     }
 
     if ($(".birthDate").length) {
@@ -79,6 +14,14 @@ $(document).ready(function() {
             dateFormat: 'd MM yyyy',
         });
     }
+
+    $('.header__tomorrow-text').on('click', function() {
+        var date = new Date();
+        date.setDate(date.getDate() + (+$(this).attr('day')));
+        datepicker.data('datepicker').selectDate(date);
+    });
+
+    $('.modalbox').fancybox();
 });
 $(".scheme__tile").on("click", function() {
     this.classList.toggle("scheme__tile--selected");
@@ -95,5 +38,6 @@ $('.map__point').on("click", function(){
 $('#select-search').selectize();
 
 $('.map__search').on("click",function(){
-var evt = new KeyboardEvent('keydown', {'keyCode':8, 'which':8});
-document.dispatchEvent(evt);});
+    var evt = new KeyboardEvent('keydown', {'keyCode':8, 'which':8});
+    document.dispatchEvent(evt);
+});
